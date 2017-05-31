90a19725df093e134ccf6f2b74f46fc4b6d6c067
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-05-31 14:22:04 +0100

    adds version 0

--- index.js
+++ index.js
@@ -1,7 +1,115 @@
-'use strict'
+const getGraph = schema => {
+  const { types } = schema
 
-module.exports = function lazyGraph (options) {
-  options = options || {}
+  const fetchTypeSchema = name =>
+    types.filter(typeSchema => typeSchema.name === name)[0]
+  const queries = []
 
-  return true
+  const objToQuery = (o) => {
+    const keys = Object.keys(o)
+    if (keys.length === 0) return ''
+    return `{ ${keys
+      .map(key => `${key} ${objToQuery(o[key])}`)
+      .join(' ')}} `
+  }
+
+  const buildQueryFromPaths = queryParts => {
+    const out = {}
+    queryParts.forEach(sections => {
+      let tip = out
+      let nextTip
+
+      sections.forEach((section, i) => {
+        const target = tip[section]
+        if (target) {
+          nextTip = target
+          tip = nextTip
+          return
+        }
+        nextTip = {}
+        tip[section] = nextTip
+        tip = nextTip
+      })
+    })
+    return objToQuery(out)
+  }
+
+  const graph = (schema, path = [], getterPath = []) => {
+    const { kind } = schema
+    queries.push(path)
+
+    if (kind === 'OBJECT') return node(schema, path, getterPath)
+
+    if (kind === 'SCALAR') return leaf(schema, path, getterPath)
+
+    if (kind === 'LIST') return seq(schema, path, getterPath)
+
+    throw new Error('could not create node for kind ', kind)
+  }
+
+  const node = (schema, path, getterPath) => {
+    const self = {}
+
+    fetchTypeSchema(schema.name).fields.forEach(field => {
+      const { name, type } = field
+      Object.defineProperty(
+        self,
+        name,
+        {
+          get: () => graph(type, path.concat(name), getterPath.concat(name)),
+          enumerable: true
+        }
+      )
+    })
+
+    const resolve = data => getIn(getterPath, data)
+    self.$resolve = resolve
+
+    return self
+  }
+
+  const getIn = ([key, ...path], o) => {
+    const next = o[key]
+    if (next === undefined || typeof next !== 'object') return next
+
+    if (path.length === 0) return next
+
+    return getIn(path, next)
+  }
+
+  const leaf = (schema, path, getterPath) => {
+    const transforms = []
+    const self = {}
+
+    const applyTransform = (result, transform) => transform(result)
+    const resolve = data =>
+      transforms.reduce(applyTransform, getIn(getterPath, data))
+    self.$resolve = resolve
+
+    const ap = f => { transforms.push(f); return self }
+    self.$ap = ap
+
+    return self
+  }
+
+  const seq = (schema, path, getterPath) => {
+    let mapped = graph(schema.ofType, path, getterPath)
+    const self = {}
+
+    const map = f => { mapped = f(mapped); return self }
+    self.$map = map
+
+    const resolve = data => getIn(getterPath, data)
+      .map(data => mapped.$resolve(data))
+    self.$resolve = resolve
+
+    return self
+  }
+
+  return {
+    graph: graph(schema.types[0]),
+    getQuery: () => buildQueryFromPaths(queries).trim()
+  }
 }
+
+module.exports = getGraph
--- test.js
+++ test.js
@@ -1,8 +1,41 @@
-import test from 'ava'
-import lazyGraph from './'
+const test = require('tape')
+const { graphql } = require('graphql')
+const { buildSchema, introspectionQuery } = require('graphql/utilities')
+
+const lazyGraph = require('./')
+
+const getter = (key, ...args) => o =>
+  args.length === 0 ? o[key] : getter(...args)(o[key])
+
+const getGraph = schema => graphql(buildSchema(schema), introspectionQuery)
+  .then(getter('data', '__schema'))
+  .then(lazyGraph)
 
 test('lazy-graph does something awesome', t => {
-  t.plan(1)
+  getGraph('type Query { aNumber: Int }').then(({ graph, getQuery }) => {
+    t.deepEqual(getQuery(), '', 'query can be empty')
+    t.equal(
+      graph.$resolve({}),
+      undefined,
+      'resolving top graph returns undefined'
+    )
 
-  t.true(lazyGraph())
+    const { aNumber } = graph
+
+    t.equal(getQuery(), '{ aNumber }', 'accessing prop creates basic query')
+
+    t.equal(
+      aNumber.$resolve({}),
+      undefined,
+      'resolving empty object on leaf gives undefined'
+    )
+
+    t.equal(
+      aNumber.$resolve({ aNumber: 1 }),
+      1,
+      'resolving object also gets the correct result'
+    )
+
+    t.end()
+  })
 })
